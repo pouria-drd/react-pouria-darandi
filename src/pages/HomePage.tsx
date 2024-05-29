f4f3f509716d0d56f3d8b323c597b3e9ad1bdca6
Pour1a-D <pour1a.dra80@gmail.com>
2024-05-29 14:07:26 +0330

    added site icon and removed unused fonts

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,7 @@
 function HomePage() {
     return (
         <div className="flex flex-col items-center justify-center gap-4">
-            <div className="bg-zinc-500 rounded-lg p-20">1</div>
-            <div className="bg-zinc-500 rounded-lg p-20">2</div>
-            <div className="bg-zinc-500 rounded-lg p-20">3</div>
-            <div className="bg-zinc-500 rounded-lg p-20">4</div>
-            <div className="bg-zinc-500 rounded-lg p-20">5</div>
+            <p>Hello World</p>
         </div>
     );
 }
